475fb78fe7a8a1a462d169b95dc5146493e3f661
LaDunne15 <vladilsav7772@gmail.com>
2023-07-26 22:28:06 +0300

    v.0.4.1

--- src/components/forecast/barchart/BarChart.tsx
+++ src/components/forecast/barchart/BarChart.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { getTemperature, getHumidity, getWind, getChance_of_rain } from "../../../service/getData";
-import { LineChart, Line, ResponsiveContainer, Tooltip, XAxis, Label, YAxis, Legend } from 'recharts';
+import { LineChart, Line, ResponsiveContainer, Tooltip, XAxis, Label} from 'recharts';
 
 function BarChart(props: any) {
 
@@ -26,51 +26,6 @@ function BarChart(props: any) {
         }
     }
     ,[props]);
-
-    const data = [
-        {
-          name: "Page A",
-          uv: 4000,
-          pv: 2400,
-          amt: 2400
-        },
-        {
-          name: "Page B",
-          uv: 3000,
-          pv: 1398,
-          amt: 2210
-        },
-        {
-          name: "Page C",
-          uv: 2000,
-          pv: 9800,
-          amt: 2290
-        },
-        {
-          name: "Page D",
-          uv: 2780,
-          pv: 3908,
-          amt: 2000
-        },
-        {
-          name: "Page E",
-          uv: 1890,
-          pv: 4800,
-          amt: 2181
-        },
-        {
-          name: "Page F",
-          uv: 2390,
-          pv: 3800,
-          amt: 2500
-        },
-        {
-          name: "Page G",
-          uv: 3490,
-          pv: 4300,
-          amt: 2100
-        }
-      ];
     
    return (
     <ResponsiveContainer width="100%" height={150}>
